11943d96d891e3bf4dcbd979e3244799a4fb8c22
Adrian Torres <adriantorres.gc@gmail.com>
2024-04-29 10:40:45 +0000

    new file:   src/js/component/login.js

---
+++ src/js/component/login.js
@@ -0,0 +1,18 @@
+import React, {useState } from "react";
+
+
+
+const Login = ()=>{
+
+
+    const [email,setEmail]=useState("")
+    const [pasword,setPasword]=useState(""){
+
+    }
+
+    return(
+
+        
+    )
+}
+
